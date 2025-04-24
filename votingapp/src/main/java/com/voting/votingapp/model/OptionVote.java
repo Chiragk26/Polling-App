24982cbc252f91030b88ca7f8f67a8111ee6be15
Chirag <khoalachirag26@gmail.com>
2025-04-24 20:00:11 +0530

    database configured

---
+++ votingapp/src/main/java/com/voting/votingapp/model/OptionVote.java
@@ -0,0 +1,14 @@
+package com.voting.votingapp.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class OptionVote {
+private String voteOption;
+private Long voteCount = 0L;
+}
--- votingapp/src/main/java/com/voting/votingapp/model/Poll.java
+++ votingapp/src/main/java/com/voting/votingapp/model/Poll.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 
 // 1.JPA requires NoArgsConstructor to instantiate entity object.
-// 2.If AllArgsConstructor is defined ( simply any parameterized constructor is there), then default constructor is not made by java.
+// 2.If AllArgsConstructor(a constructor will all parameters) is defined ( or simply any parameterized constructor is there), then default constructor is not made by java.
+// 3.database is connected to backend via this entity class.
+// 4.each field in entity class acts like a column and each of its object act like a row in the table.
 @Entity
 @Data
 @NoArgsConstructor
@@ -22,13 +24,6 @@ public class Poll {
 
     //@ElementCollection---> will create a diff table with pk and on the field it is. Helping us not to make a separate entity class.
     @ElementCollection
-    private List<String> options= new ArrayList<>();
-
-    @ElementCollection
-    private List<Long> votes= new ArrayList<>();
-
-
-
-
+    private List<OptionVote> options= new ArrayList<>();
 
 }
